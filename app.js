e70313f76854ecca5b5a4dfb31e44f247f1331fe
cr1of0x <imbaac@gmail.com>
2022-01-26 11:18:36 +0300

    Set CI for Heroku

--- app.js
+++ app.js
@@ -15,5 +15,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(process.env.PORT || 5000, () => {
-    console.log('Server listening on 5000 port...');
+    return true;
 })
--- routers/router.js
+++ routers/router.js
@@ -3,6 +3,6 @@ const router = express.Router()
 
 const about = router.get('/about', (req, res) => {
     res.status(200).send('Test')
-}
+})
 
 module.exports = about
