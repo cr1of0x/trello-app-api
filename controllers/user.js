b788d767639e859e8385b1e4fb70cc29e8198677
criofox <cr1of0xx@gmail.com>
2022-01-31 15:55:45 +0300

    Add email verification

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const User = require('../models/user.js')
+const transporter = require('../verification/sendEmail')
 
 const signin = async (req, res) => {
     const { email, password } = req.body
@@ -21,6 +22,29 @@ const signin = async (req, res) => {
     }
 }
 
+// const signup = async (req,res) => {
+//     const {login, email, password, confirmPassword} = req.body
+    
+//     try {
+//         const existingUser = await User.findOne({ email })
+
+//         if(existingUser) return res.status(404).json({message :'User already exists'})
+
+//         if(password !== confirmPassword) return res.status(400).json({message :"Passwords don't match"})
+
+//         const hashedPassword = await bcrypt.hash(password, 12)
+
+//         const result = await User.create({login: `${login}`, email, password: hashedPassword})
+
+//         const token = jwt.sign({email: result.email, id: result._id}, 'test', {expiresIn: '1h'})
+
+//         res.status(200).json({result, token})
+
+//     } catch(error) {
+//         res.status(500).json({ message: "Something went wrong" })
+//     }
+// }
+
 const signup = async (req,res) => {
     const {login, email, password, confirmPassword} = req.body
     
@@ -31,17 +55,36 @@ const signup = async (req,res) => {
 
         if(password !== confirmPassword) return res.status(400).json({message :"Passwords don't match"})
 
-        const hashedPassword = await bcrypt.hash(password, 12)
+        const token = jwt.sign({login, email, password}, 'email_secret', {expiresIn: '1h'})
 
-        const result = await User.create({login: `${login}`, email, password: hashedPassword})
+        const url = `http://localhost:5000/users/verification/${token}`
 
-        const token = jwt.sign({email: result.email, id: result._id}, 'test', {expiresIn: '1h'})
+        await transporter.sendMail({
+            to: email,
+            subject: 'Confirm Email',
+            html: `To confirm registration please click <a href="${url}">"here"</a>`
+        })
 
-        res.status(200).json({result, token})
+        res.status(200).json({ message : `Email send` })
 
     } catch(error) {
         res.status(500).json({ message: "Something went wrong" })
     }
 }
 
-module.exports = {signin, signup}
+const verification = async (req,res) => {
+    try {
+        const {login, email, password} = jwt.verify(req.params.token, 'email_secret')
+
+        const hashedPassword = await bcrypt.hash(password, 12)
+
+         await User.create({login, email, password: hashedPassword})
+
+        res.send('Your account sucessfully created! Now you can <a href="http://localhost:3000/login">login</a>')
+        } 
+    catch(error) {
+        res.send(error)
+        }
+}
+
+module.exports = {signin, signup, verification}
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router()
 const login = require('../controllers/user.js')
+const jwt = require('jsonwebtoken')
 
 router.post('/signup', login.signup)
 router.post('/signin', login.signin)
+router.get('/verification/:token', login.verification)
 
 module.exports = router
