e625ca003dac18dac89590a6ff06808b98fbc0d1
criofox <imbaac@gmail.com>
2022-02-01 18:18:00 +0300

    Soft commit

--- controllers/user.js
+++ controllers/user.js
@@ -22,38 +22,15 @@ const signin = async (req, res) => {
     }
 }
 
-// const signup = async (req,res) => {
-//     const {login, email, password, confirmPassword} = req.body
-    
-//     try {
-//         const existingUser = await User.findOne({ email })
-
-//         if(existingUser) return res.status(404).json({message :'User already exists'})
-
-//         if(password !== confirmPassword) return res.status(400).json({message :"Passwords don't match"})
-
-//         const hashedPassword = await bcrypt.hash(password, 12)
-
-//         const result = await User.create({login: `${login}`, email, password: hashedPassword})
-
-//         const token = jwt.sign({email: result.email, id: result._id}, 'test', {expiresIn: '1h'})
-
-//         res.status(200).json({result, token})
-
-//     } catch(error) {
-//         res.status(500).json({ message: "Something went wrong" })
-//     }
-// }
-
 const signup = async (req,res) => {
     const {login, email, password, confirmPassword} = req.body
     
     try {
         const existingUser = await User.findOne({ email })
 
-        if(existingUser) return res.status(404).json({message :'User already exists'})
+        if(existingUser) return res.status(404).json({errors : {email: 'User with such email already exists'}})
 
-        if(password !== confirmPassword) return res.status(400).json({message :"Passwords don't match"})
+        if(password !== confirmPassword) return res.status(400).json({errors : {password: 'Passwords dont match'}})
 
         const token = jwt.sign({login, email, password}, 'email_secret', {expiresIn: '1h'})
 
@@ -68,7 +45,7 @@ const signup = async (req,res) => {
         res.status(200).json({ message : `Email send` })
 
     } catch(error) {
-        res.status(500).json({ message: "Something went wrong" })
+        res.status(500).json({ error : "Something went wrong" })
     }
 }
 
@@ -78,9 +55,13 @@ const verification = async (req,res) => {
 
         const hashedPassword = await bcrypt.hash(password, 12)
 
+        const existingUser = await User.findOne({ email })
+
+        if(existingUser) return res.status(404).json({errors : {email: 'User with such email already exists'}})
+
          await User.create({login, email, password: hashedPassword})
 
-        res.send('Your account sucessfully created! Now you can <a href="http://localhost:3000/login">login</a>')
+        res.send('<h1>Your account sucessfully created! Now you can <a href="http://localhost:3000/login">login</a></h1>')
         } 
     catch(error) {
         res.send(error)
