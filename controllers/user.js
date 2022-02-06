7bb13deab60b10a795c29d4f0be1c8658bba5ba8
criofox <imbaac@gmail.com>
2022-02-06 19:24:39 +0300

    feature/TC-1

--- controllers/user.js
+++ controllers/user.js
@@ -2,17 +2,19 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const User = require('../models/user.js')
 const transporter = require('../verification/sendEmail')
+const authSchema = require('../verification/validationSchema')
+const Joi = require('joi')
 
 const signin = async (req, res) => {
     const { email, password } = req.body
     try {
         const existingUser = await User.findOne({ email });
 
-        if(!existingUser) return res.status(404).json({ message: "User doesn't exist" })
+        if(!existingUser) return res.status(422).json({ message: "User doesn't exist" })
 
         const isPasswordCorrect = await bcrypt.compare(password, existingUser.password)
 
-        if(!isPasswordCorrect) return res.status(400).json({message: "Invalid credentials"})
+        if(!isPasswordCorrect) return res.status(422).json({message: "Invalid credentials"})
 
         const token = jwt.sign({ email: existingUser.email, id: existingUser._id }, 'test', { expiresIn: '1h'})
 
@@ -26,11 +28,19 @@ const signup = async (req,res) => {
     const {login, email, password, confirmPassword} = req.body
     
     try {
+        const result = await authSchema.validateAsync(req.body)
+        console.log(result)
+
         const existingUser = await User.findOne({ email })
 
-        if(existingUser) return res.status(404).json({errors : {email: 'User with such email already exists'}})
-
-        if(password !== confirmPassword) return res.status(400).json({errors : {password: 'Passwords dont match'}})
+        if(existingUser) return res.status(422).json({error: {details:[
+            {
+                "message": "User with such email already exists!",              
+                "context": {
+                    "key": "email"
+                }
+            }
+        ]}})
 
         const token = jwt.sign({login, email, password}, 'email_secret', {expiresIn: '1h'})
 
@@ -45,13 +55,17 @@ const signup = async (req,res) => {
         res.status(200).json({ message : `Email send` })
 
     } catch(error) {
-        res.status(500).json({ error : "Something went wrong" })
+        if(error.isJoi) {res.status(422).json({ error })} else{res.status(500).json({ error })}
+        
     }
 }
 
 const verification = async (req,res) => {
     try {
+        
         const {login, email, password} = jwt.verify(req.params.token, 'email_secret')
+
+        console.log(jwt.verify(req.params.token, 'email_secret'))
 
         const hashedPassword = await bcrypt.hash(password, 12)
 
@@ -64,7 +78,7 @@ const verification = async (req,res) => {
         res.send('<h1>Your account sucessfully created! Now you can <a href="http://localhost:3000/login">login</a></h1>')
         } 
     catch(error) {
-        res.send(error)
+        res.status(200).json({errors:{token : 'Token is wrong or expired'}})
         }
 }
 
---
+++ verification/validationSchema.js
@@ -0,0 +1,60 @@
+const Joi = require('joi')
+
+const authSchema = Joi.object({
+    login: Joi.string().min(3).max(12).required().error(errors => {
+        errors.forEach(err => {
+          switch (err.code) {
+            case "string.empty":
+              err.message = "Login should not be empty!";
+              break;
+            case "string.min":
+              err.message = `Value should have at least ${err.local.limit} characters!`;
+              break;
+            case "string.max":
+              err.message = `Value should have at most ${err.local.limit} characters!`;
+              break;
+            default:
+              break;
+          }
+        });
+        return errors;
+      }),
+
+    email: Joi.string()
+    .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required().error(errors => {
+        errors.forEach(err => {
+            switch (err.code) {
+              case "string.empty":
+                err.message = "Email should not be empty!";
+                break;
+              case "string.email":
+                err.message = `You should have valid email adress`;
+                break;
+              default:
+                break;
+            }
+          });
+          return errors;
+    }),
+
+    password: Joi.string()
+    .pattern(new RegExp(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/)).error(errors => {
+        errors.forEach(err => {
+            switch (err.code) {
+              case "string.empty":
+                err.message = "Value should not be empty!";
+                break;
+              case "string.pattern.base":
+                err.message = `Passwords need 1 lower case letter 1 upper case letter and 1 service symbol`;
+                break;
+              default:
+                break;
+            }
+          });
+          return errors;
+    }),
+
+    confirmPassword: Joi.ref('password')
+})
+
+module.exports = authSchema
